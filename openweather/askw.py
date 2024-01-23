97c6a76950124a33bccd7ddf4a91e3d237ac8cee
Zoom.Quiet <zoom.quiet@gmail.com>
2024-01-23 15:57:48 +0800

    📖 DOC: can work but K

---
+++ openweather/askw.py
@@ -0,0 +1,19 @@
+import requests
+
+api_key = '91a709ebda96b152133fd14fc1c58661'
+city = input('Enter city name: ')
+url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+
+print(url)
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    temp = data['main']['temp']
+    desc = data['weather'][0]['description']
+    print(f'Temperature: {temp} K')
+    print(f'Description: {desc}')
+else:
+    print('Error fetching weather data')
+
+    
